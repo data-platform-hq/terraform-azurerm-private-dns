79c1b6f85cb1aa125b46f547b99cd811ce2f5228
owlleg6 <owlleg6@gmail.com>
2022-10-17 12:09:47 +0300

    fix: lint

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,6 @@ output "name" {
 }
 
 output "link_id" {
-  value       = var.vnet_map == {} ? [] : [ for vnet_link in azurerm_private_dns_zone_virtual_network_link.this : vnet_link.id ]
+  value       = var.vnet_map == {} ? [] : [for vnet_link in azurerm_private_dns_zone_virtual_network_link.this : vnet_link.id]
   description = "List of Virtual Network Link Ids"
 }
